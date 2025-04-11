58c22342aae79ecf2bbbabd34bfb41964391425e
Squidly271 <unraidsquid@gmail.com>
2025-04-11 18:43:47 -0400

    Reorganization of files

---
+++ copy_to_git.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+rm -rf "/tmp/GitHub/unassigned.devices/source/unassigned.devices/usr/local/emhttp/plugins/unassigned.devices/"
+mkdir -p "/tmp/GitHub/unassigned.devices/source/unassigned.devices/usr/local/emhttp/plugins/unassigned.devices/"
+
+cp /usr/local/emhttp/plugins/unassigned.devices/* /tmp/GitHub/unassigned.devices/source/unassigned.devices/usr/local/emhttp/plugins/unassigned.devices -R -v -p
+cd /tmp/GitHub/unassigned.devices/source/unassigned.devices/usr/local/emhttp/plugins/unassigned.devices
+# Delete Apple Metadata files
+find . -maxdepth 9999 -noleaf -type f -name "._*" -exec rm -v "{}" \;
+rm -f  ca.md5
+find . -type f -exec md5sum {} + > /tmp/ca.md5
+mv /tmp/ca.md5 ca.md5
+
+
