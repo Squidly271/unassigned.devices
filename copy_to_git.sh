33bd22d6e96e64e7a37f9f3310f2e0d99c754a1f
Squidly271 <unraidsquid@gmail.com>
2025-04-11 19:29:52 -0400

    Add back in deleted files

--- copy_to_git.sh
+++ copy_to_git.sh
@@ -4,6 +4,9 @@ rm -rf "/tmp/GitHub/unassigned.devices/source/unassigned.devices/usr/local/emhtt
 mkdir -p "/tmp/GitHub/unassigned.devices/source/unassigned.devices/usr/local/emhttp/plugins/unassigned.devices/"
 
 cp /usr/local/emhttp/plugins/unassigned.devices/* /tmp/GitHub/unassigned.devices/source/unassigned.devices/usr/local/emhttp/plugins/unassigned.devices -R -v -p
+cp /etc/udev/rules.d/99_persistent_unassigned.rules /tmp/GitHub/unassigned.devices/source/unassigned.devices/usr/local/emhttp/plugins/unassigned.devices/
+cp /tmp/GitHub/unassigned.devices/source/update.php /tmp/GitHub/unassigned.devices/source/unassigned.devices/usr/local/emhttp/plugins/unassigned.devices/include/
+
 cd /tmp/GitHub/unassigned.devices/source/unassigned.devices/usr/local/emhttp/plugins/unassigned.devices
 # Delete Apple Metadata files
 find . -maxdepth 9999 -noleaf -type f -name "._*" -exec rm -v "{}" \;
