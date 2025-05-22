7b20fec02e0fd4e04805d2147eb0d0161795d772
Squidly271 <unraidsquid@gmail.com>
2025-05-22 18:37:46 -0400

    Footer fix and repo reorganization

--- pkg_build.sh
+++ pkg_build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 tmpdir=/tmp/tmp.$(( $RANDOM * 19318203981230 + 40 ))
-archive="/mnt/disks/WINDOWS10_GitHub/community.applications/archive"
 
 version=$(date +"%Y.%m.%d")$1
 
@@ -11,7 +10,18 @@ chmod 0755 -R .
 cp --parents -f $(find . -type f ! \( -iname "pkg_build.sh" -o -iname "sftp-config.json"  \) ) $tmpdir/
 cd $tmpdir
 makepkg -l y -c y /tmp/GitHub/unassigned.devices/archive/unassigned.devices-${version}-x86_64-1.txz
-#rm -rf $tmpdir
-echo "MD5:"
+rm -rf $tmpdir
+echo "MD5 Unassigned Devices::"
 md5sum /tmp/GitHub/unassigned.devices/archive/unassigned.devices-${version}-x86_64-1.txz
 
+
+mkdir -p $tmpdir
+
+cd /tmp/GitHub/unassigned.devices/source/unassigned.devices.preclear/
+chmod 0755 -R .
+cp --parents -f $(find . -type f ! \( -iname "pkg_build.sh" -o -iname "sftp-config.json"  \) ) $tmpdir/
+cd $tmpdir
+makepkg -l y -c y /tmp/GitHub/unassigned.devices/archive/unassigned.devices.preclear-${version}-x86_64-1.txz
+rm -rf $tmpdir
+echo "MD5 Preclear::"
+md5sum /tmp/GitHub/unassigned.devices/archive/unassigned.devices.preclear-${version}-x86_64-1.txz
